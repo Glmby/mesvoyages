a68b64e287157b0a5602a2c8edbb32dc65bf522a
Guillaume Bouy <bouyglm@gmail.com>
2023-03-02 18:40:09 +0100

    Phase 2 : Hello world !

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,15 +4,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController {
+class AccueilController extends AbstractController {
     /**
      * @Route ("/", name="accueil")
      * @return Response
      */
     public function index(): Response{
-        return new Response('Hello world !');
+        return $this->render("pages/accueil.html.twig");
     }
 }
